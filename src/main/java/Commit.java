7c5c6e5b571f032269b48c264f1d9bf4e32081ca
Rumen Roger <rumenroger@gmail.com>
2020-07-26 19:37:23 +0100

    MVP.1 Commit class

--- src/main/java/Commit.java
+++ src/main/java/Commit.java
@@ -1,2 +1,18 @@
 public class Commit {
+
+    String description;
+    int uniqueId;
+
+    public Commit(String description, int uniqueId){
+        this.description = description;
+        this.uniqueId = uniqueId;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getUniqueId(){
+        return uniqueId;
+    }
 }
--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CommitTest {
+
+    Commit commit;
+
+    @Before
+    public void setUp(){
+        commit = new Commit("Initial commit", 001);
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("Initial commit", commit.getDescription());
+    }
+
+    @Test
+    public void hasUniqueId(){
+        assertEquals(001, commit.getUniqueId());
+    }
 }
